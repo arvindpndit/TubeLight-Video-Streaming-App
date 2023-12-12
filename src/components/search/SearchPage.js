7d8b6f7e89456cbcad1c34b57682af64bfecb6d0
arvindpndit <arvindpandit9808@gmail.com>
2023-12-12 09:24:10 +0530

    feat: play the searchvideo from search page

--- src/components/search/SearchPage.js
+++ src/components/search/SearchPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { json, useSearchParams } from "react-router-dom";
+import { Link, json, useSearchParams } from "react-router-dom";
 
 import { YOUTUBE_SEARCH_RESULTS } from "../../utils/constants";
 
@@ -18,30 +18,33 @@ const SearchPage = () => {
     );
     const json = await data.json();
     setSearchVideos(json?.items);
-    //console.log(searchVideos);
+    //console.log(searchVideos[0].id.videoId);
   }
   return (
     <div className="w-full sm:w-10/12 mx-auto mt-16">
-      {searchVideos.map((video) => (
-        <div key={video.id.videoId} className="flex  mb-2 ">
-          <img
-            className="h-52 w-96 object-cover mr-4 rounded-xl"
-            src={video?.snippet?.thumbnails.high.url}
-            alt="Video Thumbnail"
-          />
-          <div className="flex-1">
-            <h3 className="text-lg font-medium text-gray-900 mb-1 line-clamp-2">
-              {video?.snippet?.title}
-            </h3>
-            <p className="text-sm text-gray-600 mb-1 line-clamp-1">
-              {video?.snippet?.channelTitle}
-            </p>
-            <p className="text-sm text-gray-600">
-              {video?.snippet?.publishedAt}
-            </p>
-          </div>
-        </div>
-      ))}
+      {searchVideos &&
+        searchVideos.map((video) => (
+          <Link to={"/watch?v=" + video?.id?.videoId} key={video?.id?.videoId}>
+            <div className="flex p-4 bg-white  sm:mb-4 ">
+              <img
+                className="h-24 w-40 sm:h-52 sm:w-96 object-cover mr-4 rounded-lg"
+                src={video?.snippet?.thumbnails.high.url}
+                alt="Video Thumbnail"
+              />
+              <div className="flex-1">
+                <h3 className="text-base sm:text-lg font-medium text-gray-900 mb-1 line-clamp-2">
+                  {video?.snippet?.title}
+                </h3>
+                <p className="text-sm text-gray-600 mb-1 line-clamp-1">
+                  {video?.snippet?.channelTitle}
+                </p>
+                <p className="text-sm text-gray-600">
+                  {video?.snippet?.publishedAt}
+                </p>
+              </div>
+            </div>
+          </Link>
+        ))}
     </div>
   );
 };
